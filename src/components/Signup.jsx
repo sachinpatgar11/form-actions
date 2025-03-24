045f8e995b689d0309d5c1e8edcb2e9034b1ea06
sachinpatgar11 <sachinpatgar39@gmail.com>
2025-03-24 10:30:27 +0530

    usage of useactionstate hook

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,3 +1,4 @@
+import { useActionState } from "react";
 import {
   isEmail,
   isNotEmpty,
@@ -6,7 +7,7 @@ import {
 } from "../util/validation.js";
 
 export default function Signup() {
-  function signupAction(formData) {
+  function signupAction(prevFormState, formData) {
     const email = formData.get("email");
     const password = formData.get("password");
     const consfirmPassword = formData.get("confirm-password");
@@ -39,10 +40,20 @@ export default function Signup() {
     if (acquisitionChannel.length === 0) {
       error.push("Please select at least one acquisition.");
     }
+
+    if (error.length > 0) {
+      return { error };
+    }
+
+    return { error: null };
   }
 
+  const [formState, formAction, pending] = useActionState(signupAction, {
+    error: null,
+  });
+
   return (
-    <form action={signupAction}>
+    <form action={formAction}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
@@ -127,6 +138,13 @@ export default function Signup() {
         </label>
       </div>
 
+      {formState.error && (
+        <ul className="error">
+          {formState.error.map((error) => (
+            <li key={error}>{error}</li>
+          ))}
+        </ul>
+      )}
       <p className="form-actions">
         <button type="reset" className="button button-flat">
           Reset
