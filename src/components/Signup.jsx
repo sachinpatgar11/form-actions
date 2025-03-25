76c49671fc8ef0c3216756ca8e136019f375f2a5
sachinpatgar11 <sachinpatgar39@gmail.com>
2025-03-25 09:21:02 +0530

    Keeping errors along with the inputs entered

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -10,7 +10,7 @@ export default function Signup() {
   function signupAction(prevFormState, formData) {
     const email = formData.get("email");
     const password = formData.get("password");
-    const consfirmPassword = formData.get("confirm-password");
+    const confirmPassword = formData.get("confirm-password");
     const firstName = formData.get("first-name");
     const lastName = formData.get("last-name");
     const role = formData.get("role");
@@ -25,7 +25,7 @@ export default function Signup() {
     if (!isNotEmpty(password) || !hasMinLength(password, 6)) {
       error.push("You must enter a password of atleast six charecter.");
     }
-    if (!isEqualToOtherValue(password, consfirmPassword)) {
+    if (!isEqualToOtherValue(password, confirmPassword)) {
       error.push("Passwords do not match.");
     }
     if (!isNotEmpty(firstName) || !isNotEmpty(lastName)) {
@@ -42,7 +42,19 @@ export default function Signup() {
     }
 
     if (error.length > 0) {
-      return { error };
+      return {
+        error,
+        enteredValues: {
+          email,
+          password,
+          confirmPassword,
+          firstName,
+          lastName,
+          role,
+          acquisitionChannel,
+          terms,
+        },
+      };
     }
 
     return { error: null };
@@ -59,13 +71,23 @@ export default function Signup() {
 
       <div className="control">
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" name="email" />
+        <input
+          id="email"
+          type="email"
+          name="email"
+          defaultValue={formState.enteredValues?.email}
+        />
       </div>
 
       <div className="control-row">
         <div className="control">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            defaultValue={formState.enteredValues?.password}
+          />
         </div>
 
         <div className="control">
@@ -74,6 +96,7 @@ export default function Signup() {
             id="confirm-password"
             type="password"
             name="confirm-password"
+            defaultValue={formState.enteredValues?.confirmPassword}
           />
         </div>
       </div>
@@ -83,18 +106,32 @@ export default function Signup() {
       <div className="control-row">
         <div className="control">
           <label htmlFor="first-name">First Name</label>
-          <input type="text" id="first-name" name="first-name" />
+          <input
+            type="text"
+            id="first-name"
+            name="first-name"
+            defaultValue={formState.enteredValues?.firstName}
+          />
         </div>
 
         <div className="control">
           <label htmlFor="last-name">Last Name</label>
-          <input type="text" id="last-name" name="last-name" />
+          <input
+            type="text"
+            id="last-name"
+            name="last-name"
+            defaultValue={formState.enteredValues?.lastName}
+          />
         </div>
       </div>
 
       <div className="control">
         <label htmlFor="phone">What best describes your role?</label>
-        <select id="role" name="role">
+        <select
+          id="role"
+          name="role"
+          defaultValue={formState.enteredValues?.role}
+        >
           <option value="student">Student</option>
           <option value="teacher">Teacher</option>
           <option value="employee">Employee</option>
@@ -111,6 +148,9 @@ export default function Signup() {
             id="google"
             name="acquisition"
             value="google"
+            defaultChecked={formState.enteredValues?.acquisitionChannel.includes(
+              "google"
+            )}
           />
           <label htmlFor="google">Google</label>
         </div>
@@ -121,20 +161,36 @@ export default function Signup() {
             id="friend"
             name="acquisition"
             value="friend"
+            defaultChecked={formState.enteredValues?.acquisitionChannel.includes(
+              "friend"
+            )}
           />
           <label htmlFor="friend">Referred by friend</label>
         </div>
 
         <div className="control">
-          <input type="checkbox" id="other" name="acquisition" value="other" />
+          <input
+            type="checkbox"
+            id="other"
+            name="acquisition"
+            value="other"
+            defaultChecked={formState.enteredValues?.acquisitionChannel.includes(
+              "other"
+            )}
+          />
           <label htmlFor="other">Other</label>
         </div>
       </fieldset>
 
       <div className="control">
         <label htmlFor="terms-and-conditions">
-          <input type="checkbox" id="terms-and-conditions" name="terms" />I
-          agree to the terms and conditions
+          <input
+            type="checkbox"
+            id="terms-and-conditions"
+            name="terms"
+            defaultChecked={formState.enteredValues?.terms}
+          />
+          I agree to the terms and conditions
         </label>
       </div>
 
